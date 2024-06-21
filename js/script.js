f514a3fd9b1f70d23af5810f692ce2bf8de7efff
mohammadjodaki <jodakianm@gmail.com>
2024-06-21 23:30:04 +0330

    random color

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-document.getElementById('copyButton').addEventListener('click' , function(){
-    let inputbox=document.getElementById('box')
-    inputbox.select()
-    document.execCommand('copy')
-
-    let copyButton=document.getElementById('copyButton')
-    copyButton.innerText = "copied";
-})
-
-    const open = document.querySelector('#open');
-    const pop = document.querySelector('#pop');
-    const close = document.querySelector('#close');
+///// random background color /////
+let red , blue , green
+setInterval(() => {
+    red = Math.floor(Math.random()*255)
+    blue = Math.floor(Math.random()*255)
+    green = Math.floor(Math.random()*255)
+    document.body.style.backgroundColor = 'rgb('+red+','+blue+','+green+')'
+}, 3000);
+///// popup click /////  
+const open = document.querySelector('#open');
+const pop = document.querySelector('#pop');
+const close = document.querySelector('#close');
 
     open.addEventListener('click', function(){
         pop.classList.remove('hidden');
@@ -18,3 +18,11 @@ document.getElementById('copyButton').addEventListener('click' , function(){
     close.addEventListener('click', function(){
         pop.classList.add('hidden');
     });
+///// copy text /////
+    document.getElementById('copyButton').addEventListener('click' , function(){
+        let inputbox=document.getElementById('box')
+        inputbox.select()
+        document.execCommand('copy')
+        let copyButton=document.getElementById('copyButton')
+        copyButton.innerText = "copied";
+    })
